9790bf77249cfcb461bb6bfb04f2663edda5c0ff
leo01110111 <84789689+leo01110111@users.noreply.github.com>
2025-05-21 20:18:19 -0400

    Implemented PPO

---
+++ play.py
@@ -0,0 +1,18 @@
+import gymnasium as gym
+from ppo_torch import Agent
+
+
+
+env = gym.make("CartPole-v0", render_mode="human")
+agent = Agent(n_actions=env.action_space.n, input_dims=env.observation_space.shape)
+agent.load_models()
+observation, info = env.reset()
+
+episode_over = False
+while not episode_over:
+    action, _, _ =  agent.choose_action(observation) # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    episode_over = terminated
+
+env.close()
